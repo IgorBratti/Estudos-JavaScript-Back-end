0f3fd41c192b9f76256e1b1c2b59200de9e7493b
IgorBratti <97706074+IgorBratti@users.noreply.github.com>
2022-08-01 23:56:51 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+function say(texto) {
+    console.log(texto)
+}
+
+say("Igor");
+say("Henrique Bratti")
+
+function soma() {
+    return 2 + 2;
+}
+
+say(soma())
+
+// Math.round(): Faz o arredondamento (round em inglês) de um número de ponto flutuante para o inteiro mais próximo.
+
---
+++ template-string.js
@@ -0,0 +1,6 @@
+const nome = "Igor";
+const idade = 26;
+const cidade = "Maringá";
+
+const frase = `Meu nome é ${nome}, minha idade é ${idade} e moro em ${cidade}`;
+console.log(frase)
